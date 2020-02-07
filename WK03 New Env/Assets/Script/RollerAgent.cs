5693f4c1b9886b964eb16f955c5cebb58a5475b5
PrintTrd <60070034@kmitl.ac.th>
2020-02-07 12:01:08 +0700

    Prepare Scene1

---
+++ WK03 New Env/Assets/Script/RollerAgent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerAgent : Academy
+{
+    Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public Transform Target;
+    public override void AgentReset()
+    {
+        if (this.transform.position.y < 0)
+        {
+            //If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.position = new Vector3(0, 0.5f, 0);
+        }
+
+        //Move the target to a new spot
+        Target.position = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    void Update()
+    {
+        
+    }
+}
